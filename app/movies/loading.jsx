67e8d993c35177d4fe73bc6ee6f5581391c83d2f
vijaykumar <vijaykumar@vijaykumars-MacBook-Air.local>
2024-03-17 15:05:40 +0530

    loader added

---
+++ app/movies/loading.jsx
@@ -0,0 +1,8 @@
+import Loader from "@/components/Loader";
+export default function loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Loader />
+    </div>
+  );
+}
--- components/Home.jsx
+++ components/Home.jsx
@@ -1,8 +1,11 @@
-// import Navbar from "@/components/Navbar";
+import SlickCarousel from "@/components/SlickCarousel";
 import Rows from "@/components/Rows";
 export default function Home({ data }) {
   return (
     <div>
+      <div className="container">
+        <SlickCarousel />
+      </div>
       {data?.map((result, i) => (
         <Rows key={i} data={result} />
       ))}
